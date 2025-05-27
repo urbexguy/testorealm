bbd089976b6ba983edd08f01a9d60da88f5582ed
urbexguy <hansyumal@gmail.com>
2025-05-27 23:52:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const savedPreference = localStorage.getItem('islamicPreference');
+  if (savedPreference) {
+    document.getElementById('islamicPreference').value = savedPreference;
+  }
+});
+
 document.getElementById('settings-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const preference = document.getElementById('islamicPreference').value;
-    localStorage.setItem('islamicPreference', preference);
+  e.preventDefault();
   
-    alert('Preference saved!');
-  });
-  
+  const preference = document.getElementById('islamicPreference').value;
+  localStorage.setItem('islamicPreference', preference);
+
+  alert('Preference saved!');
+});
